b7e2264652d83175d9a41e9e4095958521beac84
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-08 16:22:48 +0200

    Task run()

--- src/main/java/com/springcloud/task_db/TaskDbApplication.java
+++ src/main/java/com/springcloud/task_db/TaskDbApplication.java
@@ -1,10 +1,13 @@
 package com.springcloud.task_db;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
@@ -19,6 +22,16 @@ public class TaskDbApplication {
     @Bean
     public TaskConfigurer getTaskConfigurer() {
         return new TaskConfigurer(dataSource);
+    }
+
+    @Component
+    public static class TaskDbApplicationRunner
+            implements ApplicationRunner {
+
+        @Override
+        public void run(ApplicationArguments arg0) throws Exception {
+            System.out.println("Hello World from Spring Cloud Task!");
+        }
     }
 
 }
