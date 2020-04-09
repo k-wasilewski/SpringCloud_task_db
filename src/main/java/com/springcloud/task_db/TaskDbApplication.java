1716d1bf4d34aea54a2ffc7ed5fb2a4f40f12b8f
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-09 11:32:29 +0200

    write book-service_history to file

--- src/main/java/com/springcloud/task_db/TaskDbApplication.java
+++ src/main/java/com/springcloud/task_db/TaskDbApplication.java
@@ -1,18 +1,30 @@
 package com.springcloud.task_db;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = BatchAutoConfiguration.class)
 @EnableTask
+@EnableBatchProcessing
 public class TaskDbApplication {
     @Autowired
     private DataSource dataSource;
--- src/main/java/com/springcloud/task_db/TaskListener.java
+++ src/main/java/com/springcloud/task_db/TaskListener.java
@@ -1,15 +1,34 @@
 package com.springcloud.task_db;
 
+import com.springcloud.task_db.db.entities.BookMessage;
+import com.springcloud.task_db.db.repositories.BookMessageRepository;
+import com.springcloud.task_db.db.repositories.RatingMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.task.configuration.DefaultTaskConfigurer;
 import org.springframework.cloud.task.listener.TaskExecutionListener;
 import org.springframework.cloud.task.repository.TaskExecution;
 
 import javax.sql.DataSource;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class TaskListener implements TaskExecutionListener {
+    @Autowired
+    private BookMessageRepository bookMessageRepository;
 
     public void onTaskStartup(TaskExecution te) {
-        System.out.println("Task startup");
+        List<BookMessage> bookMessageList = bookMessageRepository.findAll();
+        FileWriter writer = null;
+        try {
+             writer = new FileWriter("/home/kuba/Pulpit/projekty/SpringCloud/book-service_history.txt");
+            for(BookMessage bm: bookMessageList) {
+                writer.write(bm + System.lineSeparator());
+            }
+            writer.close();
+        } catch (IOException ioe) {
+            System.out.println("Cannot write to file");
+        }
     }
 
     public void onTaskEnd(TaskExecution te) {
