bdbf9e3227fed0ec5b24ff8ea8ef54c41f27ca6b
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-09 16:53:30 +0200

    config

--- src/main/java/com/springcloud/task_db/TaskDbApplication.java
+++ src/main/java/com/springcloud/task_db/TaskDbApplication.java
@@ -1,14 +1,5 @@
 package com.springcloud.task_db;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -24,7 +15,6 @@ import javax.sql.DataSource;
 
 @SpringBootApplication(exclude = BatchAutoConfiguration.class)
 @EnableTask
-@EnableBatchProcessing
 public class TaskDbApplication {
     @Autowired
     private DataSource dataSource;
--- src/main/java/com/springcloud/task_db/db/controllers/TaskController.java
+++ src/main/java/com/springcloud/task_db/db/controllers/TaskController.java
@@ -4,19 +4,9 @@ import com.springcloud.task_db.db.entities.BookMessage;
 import com.springcloud.task_db.db.entities.RatingMessage;
 import com.springcloud.task_db.db.repositories.BookMessageRepository;
 import com.springcloud.task_db.db.repositories.RatingMessageRepository;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
